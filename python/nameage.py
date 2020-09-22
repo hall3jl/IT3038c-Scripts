5413e2a1f2b02d7aa88a3d60ca4eea7c17835337
hall3jl <hall3jl@mail.uc.edu>
2020-09-21 20:57:16 -0400

    added time module and wait command

--- python/nameage.py
+++ python/nameage.py
@@ -1,6 +1,10 @@
+import time
+
 print("What is your name?")
 myName = input()
 print("Hello, " + myName + ". That is a good name. How old are you?")
 myAge = int(input())
 print("%s? That’s funny, I'm only a few seconds old." % myAge)
 print("I wish I was %s years old." % (myAge * 2))
+time.sleep(3)
+print("I'm tired. I'm going to sleep now")
