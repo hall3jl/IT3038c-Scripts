c75f83f5260fbeebd36d1b5561a4c4cbfe4380c2
hall3jl <hall3jl@mail.uc.edu>
2020-09-21 20:47:31 -0400

    added nameage.py

---
+++ python/nameage.py
@@ -0,0 +1,5 @@
+print("What is your name?")
+myName = input()
+print("Hello, " + myName + ". That is a good name. How old are you?")
+myAge = input()
+print(myAge+"? That’s funny, I'm only a few seconds old.")
