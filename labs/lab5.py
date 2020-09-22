9c0a63db1bf51f57835c2191c62b0303fe9386bb
hall3jl <hall3jl@mail.uc.edu>
2020-09-22 00:09:14 -0400

    modified inputs

--- labs/lab5.py
+++ labs/lab5.py
@@ -2,19 +2,16 @@ import random
 
 randint = random.randint(1,10)
 
-print("Guess a number between 1 and 10:")
-guess = int(input())
+guess = int(input("Guess a number between 1 and 10: "))
 
 while guess != randint:
     if randint < guess:
-        print("Your guess: " + str(guess) + " is too high")
-        print("Please guess again:")
-        guess = int(input())
+        print("Your guess, " + str(guess) + ", is too high")
+        guess = int(input("Please guess again: "))
         print("")
     elif randint > guess:
-        print("Your guess: " + str(guess) + " is too low")
-        print("Please guess again:")
-        guess = int(input())
+        print("Your guess, " + str(guess) + ", is too low")
+        guess = int(input("Please guess again: "))
         print("")
     
 print("Congratulations! %s is the correct number." % guess)
