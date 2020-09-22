5eb865c7721baebc68ec854d3dbb529aa6377d3d
hall3jl <hall3jl@mail.uc.edu>
2020-09-21 21:07:44 -0400

    added age logic

--- python/nameage.py
+++ python/nameage.py
@@ -5,6 +5,17 @@ print("What is your name?")
 myName = input()
 print("Hello, " + myName + ". That is a good name. How old are you?")
 myAge = int(input())
+if myAge < 13:
+    print("You're a minor?")
+elif myAge == 13:
+    print("You're a teenager now. Nice.")
+elif myAge > 13 and myAge < 30:
+    print("Enjoy your liver while you can.")
+elif myAge >= 30 and myAge < 65:
+    print("Go tell some kids to get off your lawn.")
+else:
+    print("You're not dead yet?")
+
 programAge = int(time.time() - start_time)
 print("%s? That’s funny, I'm only %s seconds old." % (myAge, programAge))
 print("I wish I was %s years old." % (myAge * 2))
