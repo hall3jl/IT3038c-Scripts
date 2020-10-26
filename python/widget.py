d4b2fedfb79f202c0f940a4565d71c22e8de1233
jhall1450 <43822010+jhall1450@users.noreply.github.com>
2020-10-26 02:10:17 -0400

    edited syntax for print statement

--- python/widget.py
+++ python/widget.py
@@ -4,4 +4,4 @@ import requests
 r = requests.get("http://localhost:3000")
 data=r.json()
 for i in data:
-    print(i["name"] + " is " + i["color"])
+    print("%s is %s." % (i["name"], i["color"]))
